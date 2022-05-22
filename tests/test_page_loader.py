d8059d6bf09b5c2bfb78706e10829bdfd45aba1e
Nedashkovsky <Nedashkovsky@ymail.com>
2022-05-22 22:18:48 +0300

    tests fixes

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -9,7 +9,7 @@ def test_generator_1(requests_mock, make_url_1, make_response_1):
     requests_mock.get(make_url_1, text=expected)
     with tempfile.TemporaryDirectory() as temp_dir:
         os.chdir(temp_dir)
-        result = downloader.download(make_url_1)
+        result = downloader.download(make_url_1)[0]
         with open(result, 'r') as result:
             assert expected == result.read()
 
@@ -22,22 +22,22 @@ def test_generator_2(requests_mock, make_url_1, make_response_1):
         os.chdir(temp_dir)
         with tempfile.TemporaryDirectory(dir=temp_dir,
                                          suffix='_inner_dir') as inner_temp_dir:
-            result = downloader.download(make_url_1, inner_temp_dir)
+            result = downloader.download(make_url_1, inner_temp_dir)[0]
             with open(result, 'r') as result:
                 assert expected == result.read()
 
 
-def test_make_file_name_1(make_url_1, make_url_transformed_1):
-    assert downloader.make_file_name(make_url_1) == make_url_transformed_1
+def test_make_html_name_1(make_url_1, make_url_transformed_1):
+    assert downloader.make_html_name(make_url_1) == make_url_transformed_1
 
 
-def test_make_file_name_2(make_url_2, make_url_transformed_2):
-    assert downloader.make_file_name(make_url_2) == make_url_transformed_2
+def test_make_html_name_2(make_url_2, make_url_transformed_2):
+    assert downloader.make_html_name(make_url_2) == make_url_transformed_2
 
 
-def test_make_file_name_3(make_url_3, make_url_transformed_3):
-    assert downloader.make_file_name(make_url_3) == make_url_transformed_3
+def test_make_html_name_3(make_url_3, make_url_transformed_3):
+    assert downloader.make_html_name(make_url_3) == make_url_transformed_3
 
 
-def test_make_file_name_4(make_url_4, make_url_transformed_4):
-    assert downloader.make_file_name(make_url_4) == make_url_transformed_4
+def test_make_html_name_4(make_url_4, make_url_transformed_4):
+    assert downloader.make_html_name(make_url_4) == make_url_transformed_4
