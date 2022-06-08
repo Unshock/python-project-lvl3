7e9b644a42a3106bef2c9924d362fb4a97d31944
Nedashkovsky <Nedashkovsky@ymail.com>
2022-06-08 22:08:07 +0300

    some logging updates

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -1,3 +1,4 @@
+import logging
 import re
 import requests
 import os
@@ -84,27 +85,41 @@ def download(url_, download_folder='cwd'):
     return new_file.name, download_folder
 
 
+def make_file_link(scheme, netloc, path):
+    return f'{scheme}://{netloc}{path}'
+
+
 def download_file(page_url, sub_page, dir_path):
     file_name = make_file_name(page_url, sub_page)
+    scheme = urlparse(page_url).scheme
+    page_netloc = get_netloc(page_url)
+    sub_page_netloc = get_netloc(sub_page)
+    path = urlparse(sub_page).path
+
+    if page_netloc == sub_page_netloc:
+        file_link = make_file_link(scheme, sub_page_netloc, path)
+    else:
+        file_link = make_file_link(scheme, page_netloc, path)
+
+    logging.info(f'trying to download file: {file_link} with name {file_name}')
     try:
-        response = requests.get(page_url.strip('/') + sub_page)
+        response = requests.get(file_link)
     except Exception:
+        logging.error(f'file {file_link}'
+                      f' can\'t be downloaded, return None!')
         return None
     file_path = pathlib.Path(dir_path, file_name)
     with open(file_path, 'wb') as new_file:
         new_file.write(response.content)
+    logging.info(f'file {file_name} downloaded in {dir_path}')
     return new_file.name
 
 
 def has_files(page_url, html):
-    list_of_checks = [
-        return_pics_or_none,
-        return_links_or_none,
-        return_scripts_or_none,
-    ]
+    tags = ['img', 'link', 'script']
     result = []
-    for check in list_of_checks:
-        check_result = check(page_url, html)
+    for tag in tags:
+        check_result = return_files_or_none(page_url, html, tag)
         if check_result is not None:
             result.extend(check_result)
     return result if len(result) > 0 else None
@@ -114,40 +129,63 @@ def is_valid_file_path(page_url, file_path):
     if get_netloc(page_url) == get_netloc(file_path):
         return True
     if not get_netloc(file_path):
-        return True if get_path(file_path) else False
+        # print(file_path)
+        # print(get_path(file_path))
+        if str(get_path(file_path)).startswith('/'):
+            return True
+        else:
+            logging.warning(
+                f'file {file_path} is not valid and won\'t be downloaded')
+            return False
     return False
 
 
-def return_links_or_none(page_url, html):
+def return_files_or_none(page_url, html, tag):
+    attributes_dict = {
+        'link': 'href',
+        'img': 'src',
+        'script': 'src'
+    }
     soup = BeautifulSoup(html, features='html.parser')
     result = []
-    for link_tag in soup.find_all('link'):
-        link = link_tag['href']
-        if is_valid_file_path(page_url, link):
+    for link_tag in soup.find_all(tag):
+        link = link_tag.get(attributes_dict[tag])
+        if link is None:
+            continue
+        if is_valid_file_path(page_url, link) is True:
             result.append(link)
     return result if len(result) > 0 else None
 
 
-def return_pics_or_none(page_url, html):
-    soup = BeautifulSoup(html, features='html.parser')
-    result = []
-    for img_tag in soup.find_all('img'):
-        pic = img_tag['src']
-        if is_valid_file_path(page_url, pic):
-            result.append(pic)
-    return result if len(result) > 0 else None
-
-
-def return_scripts_or_none(page_url, html):
-    soup = BeautifulSoup(html, features='html.parser')
-    result = []
-    for script_tag in soup.find_all('script'):
-        script = script_tag.get('src')
-        print(script)
-        if script and is_valid_file_path(page_url, script):
-            print(is_valid_file_path(page_url, script))
-            result.append(script)
-    return result if len(result) > 0 else None
+# def return_links_or_none(page_url, html):
+#     soup = BeautifulSoup(html, features='html.parser')
+#     result = []
+#     for link_tag in soup.find_all('link'):
+#         link = link_tag.get('href')
+#         if is_valid_file_path(page_url, link) is True:
+#             result.append(link)
+#     return result if len(result) > 0 else None
+#
+#
+# def return_pics_or_none(page_url, html):
+#     soup = BeautifulSoup(html, features='html.parser')
+#     result = []
+#     for img_tag in soup.find_all('img'):
+#         pic = img_tag.get('src')
+#         if is_valid_file_path(page_url, pic) is True:
+#             result.append(pic)
+#     return result if len(result) > 0 else None
+#
+#
+# def return_scripts_or_none(page_url, html):
+#     soup = BeautifulSoup(html, features='html.parser')
+#     result = []
+#     for script_tag in soup.find_all('script'):
+#         script = script_tag.get('src')
+#         #print(script)
+#         if is_valid_file_path(page_url, script) is True:
+#             result.append(script)
+#     return result if len(result) > 0 else None
 
 
 def is_link_of_path(string: str):
@@ -161,6 +199,7 @@ def substitution(html_path, sub_page_to_replace, full_file_name):
     with open(html_path, 'w') as html:
         x = x.replace(sub_page_to_replace, full_file_name)
         html.write(x)
+    logging.info(f'name {sub_page_to_replace} replaced with {full_file_name}')
 
 
 # t = download('https://en.wikipedia.org/wiki/Finland_me
--- page_loader/loader_engine.py
+++ page_loader/loader_engine.py
@@ -1,9 +1,16 @@
 from page_loader import downloader
 import os
+import logging
 
 
 def loader_engine(page_url, download_folder='cwd',
                   file_loader=downloader.download_file):
+    logging.basicConfig(filename='loader.log', level=logging.INFO,
+                        format='%(asctime)s %(levelname)s: %(message)s',
+                        datefmt='%d/%m/%Y %I:%M:%S')
+    logging.info('------------------------')
+    logging.info(f'Start loader_engine {page_url}')
+    logging.info('------------------------')
     html_path, download_folder = downloader.download(page_url, download_folder)
     with open(html_path) as html:
         files_sub_pages = downloader.has_files(page_url, html.read())
@@ -16,11 +23,15 @@ def loader_engine(page_url, download_folder='cwd',
                                                                     sub_page))
             file_loader(page_url, sub_page, dir_path)
             downloader.substitution(html_path, sub_page, full_file_name)
+    logging.info('------------------------')
+    logging.info('Finish loader_engine')
+    logging.info('------------------------')
     return html_path
 
-# g = 'https://page-loader.hexlet.repl.co/'
-# flib = 'https://flibusta.club/'
-# gs = 'https://gs-labs.ru/'
-# bio = 'https://bioline.ru/biomebel'
+
+g = 'https://page-loader.hexlet.repl.co/'
+flib = 'https://flibusta.club/'
+gs = 'https://gs-labs.ru/'
+bio = 'https://bioline.ru/biomebel'
 # hex = 'https://ru.hexlet.io/courses'
-# t = loader_engine(g, '/home/victor/python/test')
+# t = loader_engine(gs, '/home/victor/python/test')
